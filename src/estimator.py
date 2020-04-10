9c716f43ae850c227f2bf71dfc13b7b50416dba2
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-04-10 05:12:07 +0300

    Added get_current_infected_estimation method

--- src/estimator.py
+++ src/estimator.py
@@ -2,7 +2,18 @@
 class Estimator(object):
 
   def __init__(self,data=None):
-    pass
+    if data:
+      self.input_data = data
+    else:
+      self.input_data = {}
+
+
+  def get_current_infected_estimation(self):
+    reported_cases = self.input_data.get("reportedCases")
+    if reported_cases != None:
+      return reported_cases*10
+    else:
+      return 0
 
 
 
