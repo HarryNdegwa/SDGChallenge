b39ccc7d5baf836b0d4ad1128c0d273c647e12f2
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-04-10 05:19:57 +0300

    Added reported_cases instance variable

--- src/estimator.py
+++ src/estimator.py
@@ -7,23 +7,41 @@ class Estimator(object):
     else:
       self.input_data = {}
 
+    self.reported_cases = self.input_data.get("reportedCases")
+
 
   def get_current_infected_estimation(self):
-    reported_cases = self.input_data.get("reportedCases")
-    if reported_cases != None:
-      return reported_cases*10
+    if self.reported_cases != None:
+      return self.reported_cases*10
     return 0
 
 
   def get_severe_current_infected_estimation(self):
-    reported_cases = self.input_data.get("reportedCases")
-    if reported_cases != None:
-      return reported_cases*50
+    if self.reported_cases != None:
+      return self.reported_cases*50
     return 0
 
 
 
+payload = {
+    "region":{
+        "name":"Africa",
+        "avgAge":19.7,
+        "avgDailyIncomeInUSD":5,
+        "avgDailyIncomePopulation":0.71
+    },
+    "periodType":"Days",
+    "timeToElapse":58,
+    "reportedCases":674,
+    "population":66622705,
+    "totalHospitalBeds":1380614
+}
 
+
+e = Estimator(payload)
+
+print(e.get_current_infected_estimation())
+print(e.get_severe_current_infected_estimation())
 
 
 
